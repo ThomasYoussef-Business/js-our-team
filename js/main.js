3e768a1c7220ff0ad36acb31bc48df8a98f6d05f
ThomasYoussef-Business <thomasyoussef.business@protonmail.com>
2022-11-17 16:03:14 +0100

    Inizializzata repo

---
+++ js/main.js
@@ -0,0 +1,10 @@
+"use strict";
+/**
+ * Estratto: Devo creare un array di oggetti per i dipendenti della squadra,
+ * ognuno con le informazioni fornite. Dopodichè, devo stampare nella console le informazioni
+ * di ogni dipendente, cioè nome, ruolo, e la stringa della foto.
+ * Come ultimo obbiettivo, devo mostrare queste informazioni nel DOM come stringhe.
+ * BONUS 1 e 2: Trasformare la foto da stringa ad immagine, ed organizzare ogni membro
+ * nella sua unica scheda.
+ */
+
